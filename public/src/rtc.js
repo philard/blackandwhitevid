e783d313b18140d629809e8c587ecbc2ab376351
Philip Ardley <philard@gmail.com>
2016-04-06 19:06:39 +0800

    yeah its back and white

--- public/src/rtc.js
+++ public/src/rtc.js
@@ -12,10 +12,22 @@ navigator.webkitGetUserMedia(options , (stream) => {
 function animate(timestamp) {
     requestAnimationFrame(animate);
     ctx.drawImage(video, 0, 0);
+
     let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
     let d = pixels.data;
 
-    
+    for(let x = 0; x < d.length; x += 4) {
+        let r = d[x];
+        let g = d[x + 1];
+        let b = d[x + 2];
+
+        let grayscale = 0.2126+r + 0.7152+g + 0.0722+b;
+        let thres = grayscale > 128 ? 255 : 0;
+        d[x] = d[x + 1] = d[x + 2] = thres;
+    }
+
+    ctx.putImageData(pixels, 0, 0);
+
 }
 requestAnimationFrame(animate);
 
