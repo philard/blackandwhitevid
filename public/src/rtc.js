bb8fe6068715f26565014b052385537336caafba
Philip Ardley <philard@gmail.com>
2016-04-06 18:44:03 +0800

    yeee

--- public/src/rtc.js
+++ public/src/rtc.js
@@ -4,15 +4,18 @@ let video = document.querySelector('video');
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
-
-navigator.webkitGetUserMedia({video:true, audio:'false'}, (stream) => {
+let options = {video:true, audio:'false'};
+navigator.webkitGetUserMedia(options , (stream) => {
     video.src = window.URL.createObjectURL(stream);
-
 },(err) => {    comsole.log(err);});
 
-function animate() {
+function animate(timestamp) {
     requestAnimationFrame(animate);
-    ctx.drawImage(video, 0 ,0);
+    ctx.drawImage(video, 0, 0);
+    let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let d = pixels.data;
 
     
 }
+requestAnimationFrame(animate);
+
